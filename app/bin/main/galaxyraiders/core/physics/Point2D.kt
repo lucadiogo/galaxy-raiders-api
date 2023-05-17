6b9c4a07e67d82731a9f2af9c8860d84fcd48c7e
lucadiogo <lucadiogo11@gmail.com>
2023-05-17 14:05:11 -0300

    fixes normalization, vector-point sum and projection

---
+++ app/bin/main/galaxyraiders/core/physics/Point2D.kt
@@ -0,0 +1,40 @@
+@file:Suppress("UNUSED_PARAMETER") // <- REMOVE
+package galaxyraiders.core.physics
+
+data class Point2D(val x: Double, val y: Double) {
+  operator fun plus(p: Point2D): Point2D {
+    return Point2D(this.x + p.x, this.y + p.y)
+  }
+
+  operator fun plus(v: Vector2D): Point2D {
+    return Point2D(this.x + v.dx, this.y + v.dy)
+  }
+
+  override fun toString(): String {
+    return "Point2D(x=$x, y=$y)"
+  }
+
+  fun toVector(): Vector2D {
+    return Vector2D(this.x, this.y)
+  }
+
+  fun impactVector(p: Point2D): Vector2D {
+    return Vector2D(Math.abs(this.x - p.x), Math.abs(this.y - p.y))
+  }
+
+  fun impactDirection(p: Point2D): Vector2D {
+    return this.impactVector(p).unit
+  }
+
+  fun contactVector(p: Point2D): Vector2D {
+    return this.impactVector(p).normal
+  }
+
+  fun contactDirection(p: Point2D): Vector2D {
+    return this.contactVector(p).unit
+  }
+
+  fun distance(p: Point2D): Double {
+    return Math.sqrt((p.x - this.x) * (p.x - this.x) + (p.y - this.y) * (p.y - this.y))
+  }
+}
